00421b9f4d1ca83c184a3ca5d17d000fb48044d5
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-01-30 14:18:37 -0300

    Adicionado classe model para processo

---
+++ backend/src/main/java/br/com/softplan/desafio/models/Processo.java
@@ -0,0 +1,42 @@
+package br.com.softplan.desafio.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Processo {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "processo_seq")
+    @SequenceGenerator(name = "processo_seq", sequenceName = "processo_seq", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    @NotNull(message = "Campo usuário é obrigatório")
+    private Usuario usuario;
+
+    @NotNull(message = "Campo solicitante é obrigatório")
+    @Size(max = 50, message = "Campo solicitante deve ter no máximo {max} caracteres")
+    private String solicitante;
+
+    @NotNull(message = "Campo assunto é obrigatório")
+    @Size(max = 50, message = "Campo assunto deve ter no máximo {max} caracteres")
+    private String assunto;
+
+    private Date dataAbertura;
+
+    private Time horaAbertura;
+
+}
