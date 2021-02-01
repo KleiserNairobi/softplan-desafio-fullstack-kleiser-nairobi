ec7e8fcc1db538bf1b6351223d687af9d8d8e6a9
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-02-01 20:02:40 -0300

    Adicionado listagem de perfis

--- frontend/src/pages/perfil/index.js
+++ frontend/src/pages/perfil/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CabecalhoForm from '../../components/CabecalhoForm';
+import Tabela from '../../components/Tabela';
+import { useGeral } from '../../contexts/GeralCtx';
+import * as service from '../../services/PerfilService';
 
 
 export default function Perfil() {
+
+  const [dados, setDados] = useState([]);
+  const [carregando, setCarregando] = useState(false);  
+  const {id, carregar, setCarregar} = useGeral();
+
+  const colunasTabela = [
+    { title: 'ID', field: 'id', width: 20 },
+    { title: 'Tipo Perfil', field: 'tipo', width: 50 },
+    { title: 'Descrição', field: 'descricao', width: 1000 }
+  ];
+
+  // Effect para carregamento dos perfis
+  useEffect(() => {
+    if (carregar) {
+      setCarregando(true);
+      service.obtem('/perfis')
+      .then(response => {
+        setDados(response.data);
+        setCarregando(false);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      setCarregar(false);
+    }
+  }, [id, carregar, setCarregar])
+
   return (
     <div>
       <CabecalhoForm
@@ -10,6 +40,12 @@ export default function Perfil() {
         subtitulo="Listagem de todas os perfis"
         linkPagina="/perfis-form"
         tituloBotao="Adicionar"
+      />
+      <br/>
+      <Tabela
+        colunas={colunasTabela}
+        dados={dados}
+        carregando={carregando}
       />
     </div>
   )
