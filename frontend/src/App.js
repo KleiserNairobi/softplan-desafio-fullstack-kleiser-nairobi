1167e5d318b3bd19004812561a72d256245e06cf
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-02-01 14:28:38 -0300

    Alterado arquivo app.js para levar em consideração o layout de página

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 // importando provider
 import { AutenticacaoProvider } from './contexts/AutenticacaoCtx';
 
 // importando o layout e as páginas
 import Rotas from './pages';
+import Layout from './layout';
 
 function App() {
+  let location = useLocation(); 
   return (
     <AutenticacaoProvider>     
       <div>
         {
-          <Rotas />
+          location.pathname === '/login'
+          ? <Rotas />
+          : <Layout><Rotas /></Layout>
         }
       </div>
     </AutenticacaoProvider>
