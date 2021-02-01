d802cb0210cb75712d0eeadcee238847a9b43e0b
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-02-01 20:28:49 -0300

    Adicionado listagem de processos

--- frontend/src/pages/processo/index.js
+++ frontend/src/pages/processo/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CabecalhoForm from '../../components/CabecalhoForm';
-
+import Tabela from '../../components/Tabela';
+import { useGeral } from '../../contexts/GeralCtx';
+import * as service from '../../services/UsuarioService';
 
 export default function Processo() {
+
+  const [dados, setDados] = useState([]);
+  const [carregando, setCarregando] = useState(false);  
+  const {id, carregar, setCarregar} = useGeral();
+
+  const colunasTabela = [
+    { title: 'ID', field: 'id', width: 20 },
+    { title: 'Solicitante', field: 'solicitante', width: 250 },
+    { title: 'Assunto', field: 'assunto', width: 250 },
+    { title: 'Data Abertura', field: 'dataAbertura', width: 50 },
+    { title: 'Hora Abertura', field: 'horaAbertura', width: 50 },
+    { title: 'Usuário', field: 'usuario.id', width: 100 }
+  ];
+
+  // Effect para carregamento dos processos
+  useEffect(() => {
+    if (carregar) {
+      setCarregando(true);
+      service.obtem('/processos')
+      .then(response => {
+        setDados(response.data);
+        setCarregando(false);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      setCarregar(false);
+    }
+  }, [id, carregar, setCarregar])
+
   return (
     <div>
       <CabecalhoForm
@@ -11,6 +43,11 @@ export default function Processo() {
         linkPagina="/processos-form"
         tituloBotao="Adicionar"
       />
+      <Tabela
+        colunas={colunasTabela}
+        dados={dados}
+        carregando={carregando}
+      />      
     </div>
   )
 }
