56d6e3293624cb2b0aebecbd88be4f865f051734
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-02-01 21:59:55 -0300

    Adicionado css para os formulários

---
+++ frontend/src/pages/pagesCss.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const PagesCss = makeStyles((theme) => ({
+  cartaoTitulo: {
+    backgroundColor: theme.palette.primary.main,
+    '& .MuiCardHeader-title': {
+      color: theme.palette.primary.contrastText,
+      fontSize: '15px',
+    },
+    '& .MuiCardHeader-subheader': {
+      color: theme.palette.primary.contrastText,
+      opacity: '0.7',
+      fontSize: '12px',
+    },
+    '& .MuiSvgIcon-root': {
+      color: theme.palette.primary.contrastText
+    }
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+
+
+}));
+
+export default PagesCss;
