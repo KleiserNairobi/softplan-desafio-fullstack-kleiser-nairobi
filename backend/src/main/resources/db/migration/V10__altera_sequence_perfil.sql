a5dd949f2ae5baf1f3be04681b8c140d8aca607e
Kleiser Nairobi <kleiser.nairobi@gmail.com>
2021-02-02 22:10:20 -0300

    Adicionado duas novas migrations para corrigir as sequences de perfil e usuário (ao inserir os dados básicos não alterou a numeração)

---
+++ backend/src/main/resources/db/migration/V10__altera_sequence_perfil.sql
@@ -0,0 +1,2 @@
+-- altera sequence tabela perfil_usuario
+alter sequence perfil_usuario_seq restart with 2;
---
+++ backend/src/main/resources/db/migration/V11__altera_sequence_usuario.sql
@@ -0,0 +1,2 @@
+-- altera sequence tabela usuario
+alter sequence usuario_seq restart with 2;
